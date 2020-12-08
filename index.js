0fbe67bff8536dd2cda55617f5446c87d95a6647
ty schnoor <ty@schnoor.email>
2020-12-08 16:27:57 -0500

    im dumb

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on("message", (message) => {
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     if (command === 'ping') {
