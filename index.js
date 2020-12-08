da8db867e4674eb8e44f38bac8d00aa11ebe097d
ty schnoor <ty@schnoor.email>
2020-12-08 16:06:09 -0500

    add config values and formatted ping command

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const chalk = require('chalk');
 const client = new Discord.Client();
-let prefix = "!";
 
 const config = require("./config.json");
 const tokenfile = require("./token.json");
@@ -9,20 +8,20 @@ const tokenfile = require("./token.json");
 client.on("ready", () => {
     console.log(chalk.magenta("Starting up..."));
     setTimeout(function(){
-        console.log(chalk.magenta('AniSmp bot made by ty-schnoor'));
+        console.log(chalk.magenta('Ami SMP bot made by ty-schnoor'));
     },500);
     console.log(chalk.magenta("https://github.com/ty-schnoor/anismp/"));
 });
 
 client.on("message", (message) => {
     // Exit and stop if the prefix is not there or if user is a bot
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-    if (message.content.startsWith(prefix + "ping")) {
+    if (message.content.startsWith(config.prefix + "ping")) {
         message.channel.send("pong!");
     } else
-    if (message.content.startsWith(prefix + "foo")) {
-        message.channel.send("bar!");
+    if (message.content.startsWith(config.prefix + "foo")) {
+        message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
 });
 
